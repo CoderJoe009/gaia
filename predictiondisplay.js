b91c89a9e3a164ee15efd90aae5a64a642e0bbf8
Joe Kelly <140730352+CoderJoe009@users.noreply.github.com>
2024-03-02 21:38:39 +0300

    Create predictiondisplay.js

---
+++ predictiondisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PredictionDisplay({ prediction }) {
+  return (
+    <div>
+      <h2>Prediction: {prediction}</h2>
+    </div>
+  );
+}
+
+export default PredictionDisplay;
